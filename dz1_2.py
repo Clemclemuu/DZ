dd44281cd5650feebb68a41e5c65ea3a26f29933
Clement <146203095+Clemclemuu@users.noreply.github.com>
2023-11-27 13:07:27 +0300

    dz1_2.py

--- dz1_2.py
+++ dz1_2.py
@@ -9,16 +9,11 @@ from drawbot_skia.drawbot import polygon, saveImage, fill
 y = 100
 step = 75
 
-
-
-
 for i in range(10):
     x = 100
     y = y + step
     for j in range(10):
         polygon((x, y), (175+x, 300+y), (300+x, 300+y), (250+x, 192+y), (200+x, 170+y), close=True)
         x = x + step
-
-
 
 saveImage('dessin2.pdf')
